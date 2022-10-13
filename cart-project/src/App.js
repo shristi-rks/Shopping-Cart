df984b74f629d5ab75dfd2c6b24c0f85befbd1c3
shristi-rks <shristi.n08@gmail.com>
2022-10-13 11:54:16 +0300

    Add Column component

--- cart-project/src/App.js
+++ cart-project/src/App.js
@@ -1,44 +1,31 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
-import { useState } from 'react';
-import { Button, Items, MoreItems } from './components/Button'
+import Column from "./components/Column"
 
 
 function App() {
-  const [click, setClick] = useState(0);
-  const handleClick = () => {
-    setClick(click + 1)
-  }
-  const handleRemove = () => {
-    setClick(click - 1)
-  }
+ 
   return (
     <div className="App">
      <Header />
      <div id= "page">
      <div>
       <Main></Main>
-      <Button handleClick={handleClick} text="Add Item" />
-      <Items click = {click} />
-      <MoreItems handleClick={handleClick}  handleRemove={handleRemove} click = {click} />
+      <Column />
+     </div>
+     <div>
+     <Main></Main>
+      <Column />
      </div>
      <div>
       <Main></Main>
-      <Button handleClick={handleClick} text="Add Item" />
-      <Items click = {click} />
-      <MoreItems handleClick={handleClick}  handleRemove={handleRemove} click = {click} />
+      <Main></Main>
+      <Column />
      </div>
      <div>
       <Main></Main>
-      <Button handleClick={handleClick} text="Add Item" />
-      <Items click = {click} />
-      <MoreItems handleClick={handleClick}  handleRemove={handleRemove} click = {click} />
-     </div>
-     <div>
       <Main></Main>
-      <Button handleClick={handleClick} text="Add Item" />
-      <Items click = {click} />
-      <MoreItems handleClick={handleClick}  handleRemove={handleRemove} click = {click} />
+      <Column />
      </div>
      </div>
 
---
+++ cart-project/src/components/Column.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+export const Button = ({ handleClick, text }) => {
+    return <button onClick={handleClick}>{text}</button>
+};
+
+export const Items = ({click}) => {
+    console.log(click)
+    if (click === 1) {
+        return <p>{click} item added to the cart</p>
+    } else if (click > 1) {
+       return  <p>{click} items added to the cart</p>
+    }
+};
+
+export const MoreItems = ({click, handleClick, handleRemove}) => {
+    if (click >= 1) {
+        return (
+            <>
+                <button onClick={handleClick}>+</button>
+                <button onClick={handleClick}>{click}</button>
+                <button onClick={handleRemove}>-</button>
+            </>
+           
+        );
+    }
+};
+
+const Column = () => {
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+    setCount(count + 1) 
+    }
+    const handleRemove = () => {
+    setCount(count - 1)       
+    }
+    return (
+    <div>
+      <Button handleClick={handleClick} text="Add Item" />
+      <Items click = {count} />
+      <MoreItems handleClick={handleClick}  handleRemove={handleRemove} click = {count} />
+     </div>
+
+   );
+};
+export default Column;
