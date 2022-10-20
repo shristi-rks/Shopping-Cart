f0d31f5ee5268b1aa811b54db53de9e4593f774a
shristi-rks <shristi.n08@gmail.com>
2022-10-20 10:59:22 +0300

    import images

--- cart-project/src/data.js
+++ cart-project/src/data.js
@@ -1,29 +1,28 @@
-
 const data = {
     products: [
         {
             id:'1',
             name: 'Artbook',
             price: 30,
-            image: './components/images/Adventure.png'
+            image: '/images/Adventure.png'
         },
         {
             id:'2',
             name: 'Artbook',
             price: 38,
-            image: './components/images/Yellow-Lemon.png'
+            image: '/images/Yellow-Lemon.png'
         },
         {
             id:'3',
             name: 'Storybook',
             price: 35,
-            image: './components/images/Blue-Stars.png'
+            image: '/images/Blue-Stars.png'
         },
         {
             id:'4',
-            name: 'Adventure with Nelly',
+            name: "Nelly's Adventure",
             price: 40,
-            image: './components/images/Nelly.png'
+            image: '/images/Nelly.png'
         },
       
     ],
